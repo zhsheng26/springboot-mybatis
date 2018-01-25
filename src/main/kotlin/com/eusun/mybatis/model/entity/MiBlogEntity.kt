c09cdf001f002d73edeb443d0e7f630b8555ca09
zhangsheng <zhsheng26@gmail.com>
2018-01-25 15:14:02 +0800

    Alias

--- src/main/kotlin/com/eusun/mybatis/model/entity/MiBlogEntity.kt
+++ src/main/kotlin/com/eusun/mybatis/model/entity/MiBlogEntity.kt
@@ -1,7 +1,9 @@
 package com.eusun.mybatis.model.entity
 
 import com.eusun.mybatis.model.base.BaseEntity
+import org.apache.ibatis.type.Alias
 
+@Alias("MiBlog")
 data class MiBlogEntity(var id: String = "",
                         var title: String = "",
                         var content: String = "",
